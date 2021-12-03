323a073c7b1cf3a2999895e9e0c2a74d4aa3a5ca
shivam <shivamchouhan728@gmail.com>
2021-12-03 16:35:40 +0530

    allowed hosts

--- base/settings.py
+++ base/settings.py
@@ -29,7 +29,8 @@ DEBUG = config('DEBUG')
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://skyblogscommunity.herokuapp.com/"
 ]
 
 # Application definition
