bbb0888c5a8697ea513bfefe689df5b32621a468
shivam <shivamchouhan728@gmail.com>
2021-12-08 11:04:46 +0530

    migrations

--- base/settings.py
+++ base/settings.py
@@ -146,17 +146,27 @@ USE_S3 = config('USE_S3') == 'TRUE'
 
 if USE_S3:
     # aws settings
+    AWS_LOCATION = 'static'
     AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+    
     # s3 static settings
-    AWS_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'app.storage_backends.MediaStorage'
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+    STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    )
+    AWS_DEFAULT_ACL = None
     AWS_S3_REGION_NAME = 'ap-south-1'
+    
 else:
     STATIC_URL = '/static_root/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
